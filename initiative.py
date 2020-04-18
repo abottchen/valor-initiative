43cda866579d067005147dd62f89f6b6a9102213
Adam Bottchen <adam.bottchen@puppet.com>
2020-04-18 11:19:00 -0700

    Re-wrote to properly handle re-rolls on multi-way collisions

--- initiative.py
+++ initiative.py
@@ -8,33 +8,55 @@ import random
 from colorcodes import clr
 
 
-def handleReroll(initOrder, roll, creatureName):
-    dictName = str(initOrder[roll])
-    # When python prints the dict entries, the color codes are not interpreted, so we scrub them out
-    dictName = re.sub(r'\\.*?m', "", dictName)
-    print("Rolloff needed between " + creatureName +
-          " and " + dictName)
-    roll1 = random.randint(1, 10)
-    roll2 = random.randint(1, 10)
-    while(roll1 == roll2):
-        print("They tied again with " +
-              str(roll1) + "...  Re-rolling")
-        roll1 = random.randint(1, 10)
-        roll2 = random.randint(1, 10)
-    return dict([(roll1, initOrder[roll]), (roll2, creatureName)])
+def handleReroll(initOrder):
+    # initorder will be a hash of arrays
+    # 1 => [adam,simon,bob]
+    # 2 => [steve]
+    # ...
+    # if a hash key is a list of more than one thing
+    #  it becomes a hash of arrays for rerolls for each thing in the list
+    #
+    #  1 => {2 => [adam, bob], 5 => [simon]}
+    #  recurse until there are no lists of more than one person
+    # else:
+    #    the value sticks
+    for roll in initOrder.keys():
+        if len(initOrder[roll]) > 1:
+            print("Rerolling for ties on " + str(roll) + " by ", end='')
+            newRolls = {}
+            for name in initOrder[roll]:
+                print(name + ", ", end='')
+                newRoll = random.randint(1, 10)
+                addCreatureRoll(newRolls, newRoll, name)
+            handleReroll(newRolls)
+            initOrder[roll] = newRolls
+            print("")
 
 
-def printDict(d, indent=''):
+def printResult(d):
+    for i in sorted(d.keys(), reverse=True):
+        if type(d[i]) is dict:
+            printResult(d[i])
+        else:
+            print(d[i][0] + ",", end='')
+
+
+def prettyPrintResult(d, indent=''):
     for i in sorted(d.keys(), reverse=True):
         if type(d[i]) is dict:
             print(indent + str(i) + " -> ")
-            printDict(d[i], indent + "  ")
+            prettyPrintResult(d[i], indent + "  ")
         else:
-            print(indent + str(i) + " -> " + str(d[i]))
+            print(indent + str(i) + " -> " + d[i][0])
 
 
-# List comes in as creature,number,bonus,creature,number,bonus,creature,number,bonus...
-# -c "Biker:12:0,Carlito:1:0" -p "Adam:4,David:2,Max:10,Simon:6,Steve:5"
+def addCreatureRoll(d, r, c):
+    if r in d.keys():
+        d[r].append(c)
+    else:
+        d[r] = [c]
+
+
 def main(argv):
     creatures = ""
     players = ""
@@ -54,14 +76,14 @@ def main(argv):
 
     initOrder = {}
 
+    # List comes in as creature,number,bonus,creature,number,bonus,creature,number,bonus...
+    # -c "Biker:12:0,Carlito:1:0" -p "Adam:4,David:2,Max:10,Simon:6,Steve:5"
+
     for playerType in players.split(','):
         name, roll = playerType.split(":")
         name = clr.WHITE + name.strip() + clr.ENDC
         roll = int(roll.strip())
-        if roll in initOrder.keys():
-          initOrder[roll] = handleReroll(initOrder, roll, name)
-        else:
-          initOrder[roll] = name
+        addCreatureRoll(initOrder, roll, name)
 
     for creatureType in creatures.split(','):
         name, number, bonus = creatureType.split(":")
@@ -79,13 +101,13 @@ def main(argv):
             else:
                 creatureName = clr.COLORS[c] + c + " " + name + clr.ENDC
             roll = random.randint(1, 10) + int(bonus)
-            if roll in initOrder.keys():
-                initOrder[roll] = handleReroll(initOrder, roll, creatureName)
-            else:
-                initOrder[roll] = creatureName
+            addCreatureRoll(initOrder, roll, creatureName)
 
+    handleReroll(initOrder)
     print("---===== Final Order =====---")
-    printDict(initOrder)
+    prettyPrintResult(initOrder)
+    printResult(initOrder)
+    print("")
 
 
 if __name__ == "__main__":
